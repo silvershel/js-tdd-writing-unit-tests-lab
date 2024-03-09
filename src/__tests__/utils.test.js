de3c6296b112b632edeef08f2fa76924a2855e36
Shelli Martinez <shelli.mrtnz@gmail.com>
2024-03-09 13:21:16 -0500

    completed lab

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,24 @@
-// Your tests here
+import { isPalindrome } from "../utils";
+
+describe("isPalindrome", () => {
+    it("returns true if the string is a palindrome", () => {
+        const word = "raceCar";
+        const answer = isPalindrome(word);
+        expect(answer).toBe(true);
+    });
+    it("returns true if the string is a palindrome regardless of lettercase", () => {
+        const word = "rAceCar";
+        const answer = isPalindrome(word);
+        expect(answer).toBe(true);
+    });
+    it("returns false if the string is not a palindrome", () => {
+        const word = "nope";
+        const answer = isPalindrome(word);
+        expect(answer).toBe(false);
+    });
+    it("returns false if the string is empty", () => {
+        const word = "";
+        const answer = isPalindrome(word);
+        expect(answer).toBe(false);
+    });
+});
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,10 @@
-// Your code here
+const word = "racecar";
+const revWord = word.split('').reverse().join('');
+
+export function isPalindrome(word) {
+  if (word.toLowerCase() === revWord.toLowerCase()) {
+    return true;
+  } else {
+    return false;
+  }
+}
